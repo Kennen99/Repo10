f11e15d77251d8743ab007dcd3d442fec1691a43
Kennen99 <kenanmusanovic99@gmail.com>
2021-09-23 07:41:59 +0200

    Added a js file to ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "apache helicopter",
+    name: "Hans",
+    legs: 7,
+    friends: ["Greta", "John"]
+}
+function myFunction(myObj){
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction());
+module.exports = { myPet, myFunction};
